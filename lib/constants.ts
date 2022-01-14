be47dfb07ba0e5c8f0181a35b1252f62be9362c0
Suraj Kashyap <rajsuraj79@gmail.com>
2022-01-14 12:42:30 +0530

    commit on 'main'

--- lib/constants.ts
+++ lib/constants.ts
@@ -16,17 +16,17 @@
 
 export const SITE_URL = 'https://demo.vercel.events';
 export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
-export const TWITTER_USER_NAME = 'vercel';
-export const BRAND_NAME = 'ACME';
-export const SITE_NAME_MULTILINE = ['ACME', 'Conf'];
-export const SITE_NAME = 'ACME Conf';
+export const TWITTER_USER_NAME = 'snusunited';
+export const BRAND_NAME = 'SN';
+export const SITE_NAME_MULTILINE = ['Snyder', 'Nation'];
+export const SITE_NAME = 'Snyder Nation';
 export const META_DESCRIPTION =
-  'This is an open source demo that Next.js developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.';
+  'This is a fan website. Created by collaboration of marketers, designers, and developers for their mutual love towards the work of visionary director Mr. Zack Snyder.';
 export const SITE_DESCRIPTION =
-  'An interactive online experience by the community, free for everyone.';
-export const DATE = 'October 27, 2020';
-export const SHORT_DATE = 'Oct 27 - 9:00am PST';
-export const FULL_DATE = 'Oct 27th 9am Pacific Time (GMT-7)';
+  'An informative online fan community, free for everyone.';
+export const DATE = 'March 18, 2021';
+export const SHORT_DATE = 'Mar 18 - 9:00am PST';
+export const FULL_DATE = 'March 18th 9am Pacific Time (GMT-7)';
 export const TWEET_TEXT = META_DESCRIPTION;
 export const COOKIE = 'user-id';
 
@@ -36,8 +36,8 @@ export const LEGAL_URL = process.env.NEXT_PUBLIC_PRIVACY_POLICY_URL;
 export const COPYRIGHT_HOLDER = process.env.NEXT_PUBLIC_COPYRIGHT_HOLDER;
 
 export const CODE_OF_CONDUCT =
-  'https://www.notion.so/vercel/Code-of-Conduct-Example-7ddd8d0e9c354bb597a0faed87310a78';
-export const REPO = 'https://github.com/vercel/virtual-event-starter-kit';
+  'https://snydernation.notion.site/snydernation/Code-of-Conduct-487d8952a761440bb50b0ad72c6e4093';
+export const REPO = 'https://discord.com/invite/7qhMZnMU';
 export const SAMPLE_TICKET_NUMBER = 1234;
 export const NAVIGATION = [
   {
