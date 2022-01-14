fb229591cfbcd03aedd1a94807b4cc93cf6bb747
Suraj Kashyap <rajsuraj79@gmail.com>
2022-01-15 01:45:14 +0530

    commit

--- components/footer.tsx
+++ components/footer.tsx
@@ -22,13 +22,13 @@ import { COPYRIGHT_HOLDER, SITE_NAME, CODE_OF_CONDUCT, LEGAL_URL, REPO } from '@
 export function HostedByVercel() {
   return (
     <a
-      href="https://twitter.com/anselsn"
+      href="https://twitter.com/zeussn214"
       className={cn(styles['footer-link'], styles['footer-logo'])}
       target="_blank"
       rel="noopener noreferrer"
     >
       <div className={styles['secondary-text']}>Created by </div>
-      ANSEL SN
+      ZEUS SN
     </a>
   );
 }
