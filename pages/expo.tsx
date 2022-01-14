11202a0ad49006c5731af75388a9963b52aca1ee
Suraj Kashyap <rajsuraj79@gmail.com>
2022-01-14 21:53:52 +0530

    commit

--- pages/expo.tsx
+++ pages/expo.tsx
@@ -38,7 +38,7 @@ export default function ExpoPage({ sponsors }: Props) {
   return (
     <Page meta={meta}>
       <Layout>
-        <Header hero="Expo" description={meta.description} />
+        <Header hero="Work Expo" description={meta.description} />
         <SponsorsGrid sponsors={sponsors} />
       </Layout>
     </Page>
