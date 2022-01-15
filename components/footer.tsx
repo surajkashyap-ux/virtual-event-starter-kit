2859ffacf742dddb732f5f7d39314bd90e0fafe9
Suraj Kashyap <rajsuraj79@gmail.com>
2022-01-15 17:10:23 +0530

    commit

--- components/footer.tsx
+++ components/footer.tsx
@@ -15,9 +15,8 @@
  */
 
 import cn from 'classnames';
-import VercelLogo from '@components/icons/icon-platform';
 import styles from './footer.module.css';
-import { COPYRIGHT_HOLDER, SITE_NAME, CODE_OF_CONDUCT, LEGAL_URL, REPO } from '@lib/constants';
+import { COPYRIGHT_HOLDER, SITE_NAME, CODE_OF_CONDUCT, LEGAL_URL } from '@lib/constants';
 
 export function HostedByVercel() {
   return (
--- components/stage-container.tsx
+++ components/stage-container.tsx
@@ -17,11 +17,9 @@
  import useSWR from 'swr';
  import cn from 'classnames';
  import { Stage } from '@lib/types';
- import useLoginStatus from '@lib/hooks/use-login-status';
  import styles from './stage-container.module.css';
  import styleUtils from './utils.module.css';
  import ScheduleSidebar from './schedule-sidebar';
- import ConfEntry from './conf-entry';
  
  type Props = {
    stage: Stage;
