6fe36af8592db11352a9d00534568ab82cfd0109
Suraj Kashyap <rajsuraj79@gmail.com>
2022-01-15 16:17:57 +0530

    commit

--- components/stage-container.tsx
+++ components/stage-container.tsx
@@ -36,7 +36,6 @@ export default function StageContainer({ stage, allStages }: Props) {
 
   const updatedStages = response.data || [];
   const updatedStage = updatedStages.find((s: Stage) => s.slug === stage.slug) || stage;
-  const { loginStatus, mutate } = useLoginStatus();
 
   return (
     <div className={styles.container}>
